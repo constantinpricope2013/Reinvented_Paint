4f0631b823d3bbad166d906f9c2fc33ec9446066
cp203 <cp203@WIN-8.CTI.UGAL.RO>
2018-11-15 10:33:14 +0200

    1attempt

---
+++ src/reinvented_paint/Reinvented_Paint.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reinvented_paint;
+
+/**
+ *
+ * @author cp203
+ */
+public class Reinvented_Paint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
