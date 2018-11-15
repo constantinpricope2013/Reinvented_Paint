cb30e6884fe26f0b2984ea33d44e658411fdc0e3
cp203 <cp203@WIN-8.CTI.UGAL.RO>
2018-11-15 10:43:34 +0200

    2attempt

---
+++ src/reinvented_paint/Fereastra_Principala.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reinvented_paint;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author cp203
+ */
+public class Fereastra_Principala extends JFrame{
+    
+}
